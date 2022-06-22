53dee9625cf10442edb0e455618d707e6cfbee2e
Andreas Pardeike <andreas@pardeike.net>
2022-06-22 17:45:08 +0200

    adds unity asset bundles (and fake HUD)

---
+++ Originals/Rimionship/Assets/Editor/Export.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+	[MenuItem("Assets/Create AssetBundles")]
+	static void BuildStandaloneAssetBundles()
+	{
+		string path;
+
+		path = "Assets/AssetBundlesWin";
+		PreBuildDirectoryCheck(path);
+		_ = BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+		path = "Assets/AssetBundlesMac";
+		PreBuildDirectoryCheck(path);
+		_ = BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
+
+		path = "Assets/AssetBundlesLinux";
+		PreBuildDirectoryCheck(path);
+		_ = BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneLinux64);
+
+		File.Copy("Assets/AssetBundlesWin/rimionship", "../../Resources/rimionship-win");
+		File.Copy("Assets/AssetBundlesMac/rimionship", "../../Resources/rimionship-mac");
+		File.Copy("Assets/AssetBundlesLinux/rimionship", "../../Resources/rimionship-linux");
+	}
+
+
+	static void PreBuildDirectoryCheck(string directory)
+	{
+		if (!Directory.Exists(directory))
+			_ = Directory.CreateDirectory(directory);
+	}
+}
---
+++ Source/Assets.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEngine;
+using Verse;
+
+namespace Rimionship
+{
+	[StaticConstructorOnStartup]
+	public static class Assets
+	{
+		public static readonly AssetBundle assets = LoadAssetBundle();
+		public static readonly GameObject hud = assets.LoadAsset<GameObject>("HUD");
+		//public static readonly Font robotoCondRegular = assets.LoadAsset<Font>("RobotoCondensed-Regular");
+		//public static readonly Font robotoCondLight = assets.LoadAsset<Font>("RobotoCondensed-Light");
+		//public static readonly Font robotoCondBold = assets.LoadAsset<Font>("RobotoCondensed-Bold");
+
+		static Assets()
+		{
+			var runtimeHUD = Object.Instantiate(hud);
+			Object.DontDestroyOnLoad(runtimeHUD);
+		}
+
+		public static string GetModRootDirectory()
+		{
+			var me = LoadedModManager.GetMod<RimionshipMod>();
+			return me.Content.RootDir;
+		}
+
+		public static AssetBundle LoadAssetBundle()
+		{
+			var platform = "win";
+			if (Application.platform == RuntimePlatform.OSXPlayer)
+				platform = "mac";
+			if (Application.platform == RuntimePlatform.LinuxPlayer)
+				platform = "linux";
+
+			var path = Path.Combine(GetModRootDirectory(), "Resources", "rimionship-" + platform);
+			return AssetBundle.LoadFromFile(path);
+		}
+	}
+}
