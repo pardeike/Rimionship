410d62ff39b56df79bc7baadaee236489aec873e
Andreas Pardeike <andreas@pardeike.net>
2022-09-25 00:50:18 +0200

    test

--- Source/Patches.cs
+++ Source/Patches.cs
@@ -290,12 +290,15 @@ namespace Rimionship
 
 		public static void Postfix()
 		{
+			Log.Warning($"#1 {Current.Game} {Find.TickManager}");
 			if (Current.Game == null || Find.TickManager == null)
 				return;
 
+			Log.Warning($"#2 {Find.TickManager.TicksGame}");
 			if (Find.TickManager.TicksGame == 0)
 				return;
 
+			Log.Warning($"#3 {Find.World.GetComponent<CurrentTournamentState>()}");
 			var state = Find.World.GetComponent<CurrentTournamentState>();
 			if (state == null || (int)state.state == -1)
 			{
